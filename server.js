3febc2e391e2a8052eb4f16e17cad0c0a99db479
LisaMandarin <chu23001@byui.edu>
2024-11-20 18:52:54 +0800

    mongodb and mongoose setup done

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require("express")
+const mongoose = require("mongoose")
 require("dotenv").config()
 const app = express()
 const port = process.env.PORT
+const mongodbURI = process.env.MONGODB_URI
+
+if (!port) {
+    console.error("Port is not defined in the env file")
+    process.exit(1)
+}
+if (!mongodbURI) {
+    console.error("Invalid MongoDB URI")
+    process.exit(1)
+}
+mongoose
+    .connect(mongodbURI)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(error => {
+        console.error("Failed to connect to MongoDB.  Exiting...", error.message)
+        process.exit(1)
+    })
 
 app.get("/", (req, res) => {
     res.send("Hello")

f61ae486235e7adc9b01369bd8a91abf3352244e
LisaMandarin <chu23001@byui.edu>
2024-11-21 01:24:13 +0800

    findByQuery done

--- Routes/actorRoute.js
+++ Routes/actorRoute.js
@@ -3,5 +3,6 @@ const actorController = require("../controllers/actorController")
 
 actorRoute.get('/', actorController.findAll)
 actorRoute.get('/:id', actorController.findById)
+actorRoute.post('/search', actorController.findByQuery)
 
 module.exports = actorRoute
--- controllers/actorController.js
+++ controllers/actorController.js
@@ -6,12 +6,12 @@ exports.findAll = async (req, res, next) => {
     try {
         const result = await Actor.find()
         if (result.length > 0) {
-            res.status(200).json({
+            return res.status(200).json({
                 success: true,
                 data: result
             })
         } else {
-            res.status(200).json({
+            return res.status(200).json({
                 success: true,
                 data: [],
                 message: "No actors found."})
@@ -24,16 +24,53 @@ exports.findAll = async (req, res, next) => {
 exports.findById = async (req, res, next) => {
     try {
         if (!ObjectId.isValid(req.params.id)) {
-            res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: "Invalid ID format"})
         }
         const id = new ObjectId(req.params.id)
         const result = await Actor.findById(id)
         if (!result) {
-            res.status(400).json({
+            return res.status(400).json({
                 success: false,
                 message: "Actor not found"})
+        }
+        return res.status(200).json({
+            success: true,
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.findByQuery = async (req, res, next) => {
+    try {
+        const query = req.body
+        if (!query || Object.keys(query).length === 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Query parameters are required"
+            })
+        }
+
+        actorQuery = {}
+        for (const key in query) {
+            if (key === "seasons") {
+                actorQuery[key] = { $in: query[key]}
+            } else if ( key === "character") {
+                actorQuery[key] = { $regex: query[key]}
+            } else {
+                {actorQuery[key] = query[key]}
+            }
+        }
+
+        const result = await Actor.find(actorQuery)
+        if (!result || result.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No actor(s) found"
+            })
         }
         return res.status(200).json({
             success: true,

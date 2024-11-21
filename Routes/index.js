311f6559e54ecb4c18ab96bf26d747b5cd1ce594
LisaMandarin <chu23001@byui.edu>
2024-11-22 00:24:34 +0800

    change documentaionURL

--- Routes/index.js
+++ Routes/index.js
@@ -5,7 +5,7 @@ const swaggerRoute = require("./swaggerRoute")
 router.use("/api-docs", swaggerRoute)
 router.use("/actors", actorRoute)
 router.use("/", (req, res) => {
-    let docData = { documentationURL: 'http://localhost:3000/api-docs'}
+    let docData = { documentationURL: 'https://cse341-project2-h5oa.onrender.com//api-docs'}
     res.json(docData)
 })
 

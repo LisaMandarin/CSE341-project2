59ec6d6d9590c1f0a2c885b40f143c41efc07cda
LisaMandarin <chu23001@byui.edu>
2024-11-27 02:58:22 +0800

    delete console log

--- Routes/index.js
+++ Routes/index.js
@@ -5,14 +5,12 @@ const swaggerRoute = require("./swaggerRoute")
 const { requiresAuth } = require("express-openid-connect")
 
 router.get("/profile", requiresAuth(), (req, res) => {
-    console.log("profile authenticated: ", req.oidc.isAuthenticated())
     res.json(req.oidc.user)
 })
 router.use("/api-docs", swaggerRoute)
 router.use("/actors", actorRoute)
 router.use("/moments", momentRoute)
 router.use("/", (req, res) => {
-    console.log("authenticated: ", req.oidc.isAuthenticated())
     let docData = 'https://cse341-project2-h5oa.onrender.com/api-docs'
     res.send(req.oidc.isAuthenticated() ? "Logged in: " + docData : "Logged out")
 })

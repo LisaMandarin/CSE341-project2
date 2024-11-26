95103b44bd2e36c42d0cfaa7e2f61d7f87762933
LisaMandarin <chu23001@byui.edu>
2024-11-27 02:56:44 +0800

    auth done

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const mongodbURI = process.env.MONGODB_URI
 const errorHandling = require("./utils/errorHandling")
 const connectDB = require("./models/index")
 const router = require("./Routes")
+const { auth } = require("express-openid-connect")
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+}
+
+app.use(auth(config))
 
 if (!port) {
     throw new Error("Port is not defined in the env file")

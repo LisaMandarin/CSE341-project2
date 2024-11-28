647181aa355890bfc090bfdae7be3465188dbd82
LisaMandarin <chu23001@byui.edu>
2024-11-28 10:23:04 +0800

    add middleware to set response type to JSON

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ connectDB(mongodbURI)
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json")
+    next()
+})
 app.use(auth(config))
 app.use("/", router)
 app.use((req, res) => {
